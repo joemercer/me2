f7f725bad31aa3569b739fbfd9fb5e14d5db9e70
Joe Mercer <mynameisjoemercer@gmail.com>
2015-07-26 20:10:16 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp        = require('gulp'),
+    $           = require('gulp-load-plugins')(),
+    path        = require('path'),
+    browserSync = require('browser-sync'),
+    through2    = require('through2'),
+    reload      = browserSync.reload,
+    browserify  = require('browserify'),
+    del         = require('del'),
+    argv        = require('yargs').argv;
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    open: !!argv.open,
+    notify: !!argv.notify,
+    server: {
+      baseDir: "./dist"
+    }
+  });
+});
+
+gulp.task('css', function() {
+  return gulp.src('./src/css/**/*.{css,less}')
+    .pipe($.plumber())
+    .pipe($.less({
+      paths: 'src/css'
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('js', function() {
+  return gulp.src('src/scripts/*.js')
+    .pipe($.plumber())
+    .pipe(through2.obj(function (file, enc, next) {
+      browserify(file.path, { debug: true })
+        .transform(require('babelify'))
+        .transform(require('debowerify'))
+        .bundle(function (err, res) {
+          if (err) { return next(err); }
+          file.contents = res;
+            next(null, file);
+        });
+      }))
+      .on('error', function (error) {
+        console.log(error.stack);
+        this.emit('end')
+    })
+  .pipe( $.rename('app.js'))
+  .pipe( gulp.dest('dist/scripts/'));
+});
+
+
+gulp.task('clean', function(cb) {
+  del('./dist', cb);
+});
+
+gulp.task('images', function() {
+  return gulp.src('./src/images/**/*')
+    .pipe($.imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('./dist/images'))
+})
+
+gulp.task('templates', function() {
+  return gulp.src('src/*.jade')
+    .pipe($.plumber())
+    .pipe($.jade({
+      pretty: true
+    }))
+    .pipe( gulp.dest('dist/') )
+});
+
+
+
+gulp.task('build', ['css', 'js', 'templates', 'images']);
+
+gulp.task('serve', ['build', 'browser-sync'], function () {
+  gulp.watch('src/css/**/*.{css,less}',['css', reload]);
+  gulp.watch('src/scripts/**/*.js',['js', reload]);
+  gulp.watch('src/images/**/*',['images', reload]);
+  gulp.watch('src/*.jade',['templates', reload]);
+});
+
+gulp.task('default', ['serve']);
