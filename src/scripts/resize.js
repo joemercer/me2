98d3ab8e6d57d4e5a68fd085fbd603de410865c7
Joe Mercer <mynameisjoemercer@gmail.com>
2015-08-06 00:09:01 -0400

    resize effect

---
+++ src/scripts/resize.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// # Resize Icon Effect
+// ________________
+
+var $ = require('jquery');
+
+$(function(){
+
+	var $window = $(window);
+	var $icon = $('.icon-container');
+
+	$window.scroll(function(e){
+		var top = $window.scrollTop();
+
+		if (top <= 100) {
+			$icon.css('margin-top', 100 - top)
+		}
+		else {
+			$icon.css('margin-top', 0)
+		}
+
+		if (top <= 100) {
+			$icon.height(250);
+		}
+		if (top > 100 && top < 275) {
+			$icon.height(250 - top + 100);
+		}
+		if (top >= 275) {
+			$icon.height(75);
+		}
+	});
+
+});
